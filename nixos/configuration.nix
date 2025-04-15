b7e4978c10c4bbe883a5d948e8b997461a9056d5
Kristoffer Balintona <krisbalintona@gmail.com>
2025-04-15 21:42:18 +0000

    Initial transfer of NixOS config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,99 +1,288 @@
-# This is your system's configuration file.
-# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+# * Preamble
 {
   inputs,
   lib,
   config,
   pkgs,
   ...
-}: {
-  # You can import other NixOS modules here
+}:
+{
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.05";
+
+  # * Imports
   imports = [
-    # If you want to use modules from other flakes (such as nixos-hardware):
-    # inputs.hardware.nixosModules.common-cpu-amd
-    # inputs.hardware.nixosModules.common-ssd
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./users.nix
-
-    # Import your generated (nixos-generate-config) hardware configuration
-    ./hardware-configuration.nix
+    <nixos-wsl/modules> # include NixOS-WSL modules
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
+  # * Nix
+  nix =
+    let
+      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+    in
+    {
+      settings = {
+        experimental-features = "nix-command flakes"; # Enable flakes and 'nix' command
+        # flake-registry = ""; # Opinionated: disable global registry
+        nix-path = config.nix.nixPath; # Workaround for https://github.com/NixOS/nix/issues/9574
+      };
+      channel.enable = false; # Opinionated: disable channels because we manage package sources through flakes
 
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
+      # # Opinionated: make flake registry and nix path match flake inputs
+      # registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
+      # nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+    };
+
+  # * Nixpkgs
+  nixpkgs = {
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
     };
+    overlays = [ ];
   };
 
-  nix = let
-    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-  in {
-    settings = {
-      # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
-      # Opinionated: disable global registry
-      flake-registry = "";
-      # Workaround for https://github.com/NixOS/nix/issues/9574
-      nix-path = config.nix.nixPath;
-    };
-    # Opinionated: disable channels
-    channel.enable = false;
+  # * Networking
+  networking.hostName = "WSL-NixOS";
 
-    # Opinionated: make flake registry and nix path match flake inputs
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  # * WSL2
+  # See https://nix-community.github.io/NixOS-WSL/options.html.
+  wsl = {
+    enable = true;
+    defaultUser = "krisbalintona";
+    startMenuLaunchers = true;
   };
 
-  # FIXME: Add the rest of your current configuration
+  # 2025-04-12: Workaround due to a bug in WSLg causing issues using GUI tools
+  # in Wayland.  Solution taken from
+  # https://github.com/microsoft/WSL/issues/11261#issuecomment-2011901398.
+  systemd.services."user-runtime-dir@.service" = {
+    serviceConfig.ExecStart = "sh -c \"ln -fs /mnt/wslg/runtime-dir/* /run/user/%i\"";
+    serviceConfig.After = [ "wslg.target" ];
+    wantedBy = [ "wslg.target" ];
+  };
 
-  # TODO: Set your hostname
-  networking.hostName = "your-hostname";
-
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
+  # * Users
   users.users = {
-    # FIXME: Replace with your username
-    your-username = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      initialPassword = "correcthorsebatterystaple";
+    krisbalintona = {
       isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-      ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["wheel"];
+      openssh.authorizedKeys.keys = [ ];
+      extraGroups = [ "wheel" ];
     };
   };
 
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
+  # * Environment
+
+  # ** Packages
+  environment.systemPackages = with pkgs; [
+    # Utilities
+    home-manager
+    wl-clipboard
+    gcc
+    gnumake
+    pkgconf # Are these two different?
+    pkg-config
+    git
+    openssh
+    x11_ssh_askpass
+    tree
+    ripgrep
+    automatic-timezoned
+    unzip
+    xorg.xmodmap
+
+    # Wayland
+    wayland
+    wayland-utils
+
+    # Shell
+    atuin
+    neovim
+    keychain
+
+    # Fish
+    fish
+    fishPlugins.hydro
+    fishPlugins.fzf-fish
+    fzf
+    fishPlugins.grc
+    grc
+
+    # Languages and package managers
+    python3Full
+    R
+    nodejs
+    yarn
+
+    # My tools
+    syncthing
+    emacs
+    libreoffice-still
+    graphviz
+  ];
+  # *** Fonts
+  fonts = {
+    enableDefaultPackages = true;
+    fontDir.enable = true; # Flatpak compatibility
+    packages = with pkgs; [
+      google-fonts
+      noto-fonts
+      noto-fonts-extra
+      overpass
+      iosevka
+      lato
+      liberation_ttf
+      open-sans
+      roboto
+      roboto-mono
+      ubuntu-sans
+      ubuntu-sans-mono
+      jetbrains-mono
+      anonymousPro
+      fira-code
+      fira-code-symbols
+
+      # Symbols for Emacs
+      nerd-fonts.symbols-only
+      emacs-all-the-icons-fonts
+
+      # Nerd fonts.  We generally prefer nerd-font versions of fonts.
+      nerd-fonts.iosevka
+      nerd-fonts.iosevka-term
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.fira-code
+      nerd-fonts.droid-sans-mono
+      nerd-fonts.noto
+      nerd-fonts.hack
+      nerd-fonts.ubuntu
+      nerd-fonts.overpass
+      # Nerd-fonts versions of Iosevka variants.  See
+      # https://github.com/Iosevka-NerdFont for all available fonts and their
+      # respective repositories.  Nix implementation inspired by
+      # https://www.reddit.com/r/NixOS/comments/10726vc/installing_fonts_that_arent_in_nixpkgs/
+      # and https://github.com/jeslie0/fonts/blob/main/flake.nix.  (Note: the
+      # truetype/ subdirectory is conventionally where TTF fonts are held.)
+      (pkgs.stdenv.mkDerivation {
+        name = "nerd-font.iosevka-aile";
+        src = pkgs.fetchFromGitHub {
+          owner = "Iosevka-NerdFont";
+          repo = "IosevkaAile";
+          rev = "main";
+          sha256 = "sha256-QmRFp8Il05w5naWVpviJNLqXSXDDzxKLQnsrkSGfu04=";
+        };
+        installPhase = ''
+          mkdir -p $out/share/fonts/truetype/nerd-fonts-iosevka-aile
+          cp -r $src/IosevkaAile/* $out/share/fonts/truetype/nerd-fonts-iosevka-aile
+        '';
+      })
+      (pkgs.stdenv.mkDerivation {
+        name = "nerd-font.iosevka-ss11";
+        src = pkgs.fetchFromGitHub {
+          owner = "Iosevka-NerdFont";
+          repo = "IosevkaSS11";
+          rev = "main";
+          sha256 = "sha256-FXyHa1wJiexplmjtKDX/b2d/P57UURMFPaolOmO9q2s=";
+        };
+        installPhase = ''
+          mkdir -p $out/share/fonts/truetype/nerd-fonts-iosevka-ss11
+          cp -r $src/IosevkaSS11/* $out/share/fonts/truetype/nerd-fonts-iosevka-ss11
+        '';
+      })
+      (pkgs.stdenv.mkDerivation {
+        name = "nerd-font.iosevka-term-ss04";
+        src = pkgs.fetchFromGitHub {
+          owner = "Iosevka-NerdFont";
+          repo = "IosevkaTermSS04";
+          rev = "main";
+          sha256 = "sha256-4RrAIIHkuvX8/sZNrkLeOfv/uN7d89trmIa2F6xC28I=";
+        };
+        installPhase = ''
+          mkdir -p $out/share/fonts/truetype/nerd-fonts-iosevka-term-ss04
+          cp -r $src/IosevkaTermSS04/* $out/share/fonts/truetype/nerd-fonts-iosevka-term-ss04
+        '';
+      })
+    ];
+  };
+
+  # ** Variables
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1"; # Hint electron apps to use wayland
+  };
+
+  # * Services
+
+  # ** OpenSSH
+  # This setups a SSH server.  Very important if you're setting up a headless
+  # system.  Feel free to remove if you don't need it.
   services.openssh = {
     enable = true;
     settings = {
-      # Opinionated: forbid root login through SSH.
-      PermitRootLogin = "no";
-      # Opinionated: use keys only.
-      # Remove if you want to SSH using passwords
-      PasswordAuthentication = false;
+      PermitRootLogin = "no"; # Opinionated: forbid root login through SSH.
+      PasswordAuthentication = true; # Set to false if I want to use keys only
     };
   };
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "23.05";
+  # ** Syncthing
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true"; # Don't create default ~/Sync folder
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+    guiAddress = "127.0.0.1:8385"; # Different one from default (8384)
+    user = "krisbalintona";
+    group = "users";
+    #dataDir = "/home/krisbalintona/.config/syncthing";
+    configDir = "/home/krisbalintona/.config/syncthing";
+    # Override settings from GUI
+    overrideDevices = true;
+    overrideFolders = true;
+    settings = {
+      devices = {
+        "G14 2024 Arch WSL2" = {
+          id = "OQHSZRW-L2TT7IC-7USSLNU-ST7JYML-J7J6CU3-42P7NCA-WHE7BEL-SASRXA3";
+        };
+        "OnePlus 7 Pro" = {
+          id = "OVGYOBF-JPFQJKE-6CKRY7J-JULRCWK-WSGSA6Y-SQZYLLE-B2OLSDJ-6DRSTQZ";
+        };
+      };
+
+      folders = {
+        "k4vqh-rny7b" = {
+          label = "Agenda";
+          path = "/home/krisbalintona/Documents/org-database/agenda/";
+          devices = [
+            "G14 2024 Arch WSL2"
+            "OnePlus 7 Pro"
+          ];
+        };
+        "qtuzy-ufufb" = {
+          label = "Notes";
+          path = "/home/krisbalintona/Documents/org-database/notes";
+          devices = [
+            "G14 2024 Arch WSL2"
+            "OnePlus 7 Pro"
+          ];
+        };
+      };
+    };
+  };
+
+  # ** Geoclue 2
+  # 2025-04-12: Change the location provider.  A manual implementation of the
+  # fix described in this PR: https://github.com/NixOS/nixpkgs/pull/391845
+  services.geoclue2.geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
+
+  # ** Automatic-timezoned
+  # Automatically set time zone according to location.  Uses geoclue2; see that
+  # packages options for configuration of how automatic-timezoned retrieves its
+  # data.
+  services.automatic-timezoned.enable = true;
+
+  # ** Dictd
+  services.dictd.enable = true;
+  services.dictd.DBs = with pkgs.dictdDBs; [
+    # 2025-04-14: Don't know of any other good English databases
+    wordnet
+    wiktionary
+  ];
+
+  # * Programs
 }
