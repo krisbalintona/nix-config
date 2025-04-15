2a846b916bd031132f5ad3a773482bf125484288
Kristoffer Balintona <krisbalintona@gmail.com>
2025-04-15 22:13:03 +0000

    Initial transfer of home-manager config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,3 +1,4 @@
+# * Preamble
 {
   inputs,
   lib,
@@ -6,54 +7,169 @@
   ...
 }:
 {
-  # You can import other home-manager modules here
-  imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
-  ];
-
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
-    };
-  };
-
-  # TODO: Set your username
-  home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
-  };
-
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
-
-  # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
 
-  # Nicely reload system units when changing configs
+  home = {
+    username = "krisbalintona";
+    homeDirectory = "/home/krisbalintona";
+  };
+
+  # Nicely reload system units when changing configs.  See
+  # https://home-manager-options.extranix.com/?query=systemd.user.startServices&release=master
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
+
+  # * Imports
+  imports = [ ];
+
+  # * Nixpkgs
+  nixpkgs = {
+    overlays = [ ];
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true; # Workaround for https://github.com/nix-community/home-manager/issues/2942
+    };
+  };
+
+  # * Packages
+  home.packages = with pkgs; [
+    ## Emacs stuff
+    jujutsu
+    # NOTE 2025-04-12: It seems all the dicts (but not necessarily the
+    # dictionaries for the language(s) I use) need to be installed for jinx to
+    # recognize the language?  A quirk of the .c module...?
+    enchant
+    aspellDicts.en
+    nuspell
+    hunspell
+    hunspellDicts.en_US-large
+    vale
+    harper
+    yt-dlp
+    hugo
+    mermaid-cli
+    msmtp
+    notmuch
+    lieer
+    texlivePackages.latexmk
+    texliveFull # To cover every case
+    # Coding
+    go
+    emacs-lsp-booster
+    nixfmt-rfc-style
+    nodePackages_latest.prettier
+  ];
+
+  # * Files
+  # The primary way to manage plain files
+  home.file = {
+    ".config/jj/config.toml" = {
+      source = config/jj/config.toml;
+    };
+
+    ".config/enchant" = {
+      source = config/enchant;
+      recursive = true;
+    };
+
+    ".config/atuin/config.toml" = {
+      source = config/atuin/config.toml;
+      force = true; # Overwrite the default one that's automatically generated
+    };
+  };
+
+  # * Environment
+
+  # ** Variables
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # * Programs
+  programs.git = {
+    enable = true;
+    userEmail = "krisbalintona@gmail.com";
+    userName = "Kristoffer Balintona";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.ssh = {
+    enable = true;
+    addKeysToAgent = "yes";
+    forwardAgent = true;
+  };
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      atuin init fish | source
+    '';
+    plugins = with pkgs; [
+      {
+        name = "grc";
+        src = fishPlugins.grc.src;
+      }
+      {
+        name = "z";
+        src = fishPlugins.z.src;
+      }
+      {
+        name = "hydro";
+        src = fishPlugins.hydro.src;
+      }
+      {
+        name = "fzf-fish";
+        src = fishPlugins.fzf-fish.src;
+      }
+    ];
+  };
+
+  services.emacs.package = pkgs.emacsGit; # Use emacs-overlay for the emacs daemon
+  programs.emacs = {
+    enable = true;
+    package = pkgs.emacsWithPackagesFromUsePackage {
+      package = pkgs.emacs-git.overrideAttrs (old: {
+        src = pkgs.fetchFromGitHub {
+          owner = "emacs-mirror";
+          repo = "emacs";
+          rev = "8c411381c69";
+          # Can find the sha256 by either passing an empty string and seeing
+          # what home-manager switch reports the correct sha256 to be, or
+          # running in the CLI something in the form of
+          #
+          #   nix flake prefetch <source>:<owner>/<repo>/<rev>
+          #
+          # For example:
+          #
+          #   nix flake prefetch github:emacs-mirror/emacs/8c411381c69
+          sha256 = "sha256-E20O3pySvO1WmOFyyg7744sbwf50nvlPtnbwf7lWZDA=";
+        };
+        # Additional configure flags
+        configureFlags = (old.configureFlags or [ ]) ++ [
+        ];
+        # Extra build inputs
+        buildInputs = (old.buildInputs or [ ]) ++ [
+        ];
+      });
+
+      config = "/home/krisbalintona/.emacs.d/emacs-config.org";
+      defaultInitFile = false;
+      alwaysEnsure = true;
+      alwaysTangle = false;
+
+      extraEmacsPackages = epkgs: [
+        epkgs.jinx
+        epkgs.denote
+      ];
+
+      # If you want to override packages in the Emacs package set:
+      # override = final: prev: { };
+    };
+  };
+
+  # * End
 }
