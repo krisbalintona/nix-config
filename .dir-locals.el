a7d46e02f654be886cb94576a0f1fa060f1fb007
Kristoffer Balintona <krisbalintona@gmail.com>
2025-05-02 23:01:34 -0500

    .dir-locals.el: Add justfile compile-multi commands

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((compile-multi-config . (((file-exists-p "justfile") ("just:list" . "just --list")
+                                   ("just:home-manager" . "just home-manager") ("just:nixos" . "just nixos")))))))
